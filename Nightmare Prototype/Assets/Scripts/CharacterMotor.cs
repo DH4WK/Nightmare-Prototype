041e1e7b3b913dcaeeeecf833aa621590a3085cb
Daniel Hawkins <danhawkins5789@gmail.com>
2015-09-17 18:19:04 -0400

    Commit 2 - Restoring old files from previous project

---
+++ Nightmare Prototype/Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMotor : MonoBehaviour
+{
+	public Rigidbody rigidBody;
+	
+	public float acceleration = 0.01f;
+	public float speed = 3f;
+
+	public Vector3 velocityInput = Vector3.zero;
+	public Vector3 realitiveVelocity = Vector3.zero;
+	private Vector3 moveVelocity;
+	
+	public float jumpAmount = 4f;
+
+	public bool grounded = false;
+	public float slopeLimit = 60f;
+	
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 moveInput = new Vector3 (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical"));
+
+		if (moveInput.magnitude > 1f)
+		{
+			moveInput.Normalize ();
+		}
+
+		velocityInput = Vector3.SmoothDamp (velocityInput, moveInput * speed, ref moveVelocity, acceleration);
+		realitiveVelocity = Quaternion.FromToRotation (Vector3.forward, this.transform.forward) * velocityInput;
+		
+		rigidBody.velocity = new Vector3 (realitiveVelocity.x, rigidBody.velocity.y, realitiveVelocity.z);
+		
+		if (Input.GetButton ("Jump") && grounded)
+		{
+			rigidBody.AddForce (Vector3.up * jumpAmount, ForceMode.Impulse);
+		}
+	}
+	
+	void OnCollisionStay (Collision other)
+	{
+		foreach (ContactPoint c in other.contacts)
+		{
+			if (Mathf.Abs (Vector3.Angle (c.normal, Vector3.up)) < slopeLimit)
+			{
+				grounded = true;
+			}
+		}
+	}
+	
+	void OnCollisionExit ()
+	{
+		grounded = false;
+	}
+}
+
+
+
---
+++ Nightmare Prototype/Assets/Scripts/Daniel/Player Scripts/PlayerAnimationControllScript.cs
@@ -0,0 +1,29 @@
+﻿namespace Player
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public class PlayerAnimationControllScript : MonoBehaviour
+    {
+        private Animator anim;
+
+        float move;
+
+        int speedHashID = Animator.StringToHash("Speed");
+
+        // Use this for initialization
+        void Start()
+        {
+            // Get the animator
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            move = Input.GetAxis("Vertical");
+            //anim.SetFloat("Speed", move);
+            anim.SetFloat(speedHashID, move);
+        }
+    }
+}
