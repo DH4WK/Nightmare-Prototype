47578ac7321dff6592d7b504f5c58b7f05a3a1d2
Daniel Hawkins <danhawkins5789@gmail.com>
2015-09-18 00:41:35 -0400

    Commit 3 - Up and Running

--- Nightmare Prototype/Assets/Scripts/Daniel/Player Scripts/PlayerAnimationControllScript.cs
+++ Nightmare Prototype/Assets/Scripts/Daniel/Player Scripts/PlayerAnimationControllScript.cs
@@ -5,25 +5,34 @@
 
     public class PlayerAnimationControllScript : MonoBehaviour
     {
+        // The animator component
         private Animator anim;
 
+        // User input for our directions
         float move;
+        float turn;
 
+        // Animation Paramters converted to hash strings for a more efficient approach
         int speedHashID = Animator.StringToHash("Speed");
+        int turnHashID = Animator.StringToHash("Direction");
 
         // Use this for initialization
         void Start()
         {
             // Get the animator
-            anim = GetComponent<Animator>();
+            anim = this.GetComponentInChildren<Animator>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            // Getting the user input
             move = Input.GetAxis("Vertical");
-            //anim.SetFloat("Speed", move);
+            turn = Input.GetAxis("Horizontal");
+
+            // Now actually moving the animations
             anim.SetFloat(speedHashID, move);
+            anim.SetFloat(turnHashID, turn);
         }
     }
 }
