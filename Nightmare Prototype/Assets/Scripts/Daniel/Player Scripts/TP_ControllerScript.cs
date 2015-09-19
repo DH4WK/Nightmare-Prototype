441eb1e60694e1eae30e20d6684b555d261f6753
Daniel Hawkins <danhawkins5789@gmail.com>
2015-09-18 21:11:10 -0400

    Adding placeholders to the project

---
+++ Nightmare Prototype/Assets/Scripts/Daniel/Player Scripts/TP_ControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TP_ControllerScript : MonoBehaviour {
+
+    public static CharacterController player_Character_Controller;
+    public static TP_ControllerScript Instance;
+
+    void Awake()
+    {
+        player_Character_Controller = GetComponent<CharacterController>();
+
+        Instance = this;
+    }
+
+	void Update() 
+    {
+        if (Camera.main == null)
+            return;
+
+        GetLocomotionInput();
+	}
+
+    void GetLocomotionInput()
+    {
+        //
+    }
+}
